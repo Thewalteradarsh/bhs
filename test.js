72053302bad219d2840bd32e480de4f1dbe5e52f
Thewalteradarsh <beatzadgaming@gmail.com>
2026-07-20 03:01:22 +0530

    remove temporary scripts

--- test.js
+++
@@ -1,6 +0,0 @@
-﻿const fs = require('fs');
-const html = fs.readFileSync('spotify.html', 'utf8');
-
-const regex = /<meta content="([^"]+)" property="twitter:title"/g;
-let match = regex.exec(html);
-console.log(match);
